620a4a5b6bb0c0b71ef0776f9b1af193a097ce73
Aitorcin207 <a.martinlo.2022@alumnos.urjc.es>
2025-03-29 21:09:37 +0100

    se mueve todo con lag

--- P3/Canva_Centauri.js
+++ P3/Canva_Centauri.js
@@ -3,8 +3,8 @@ const canvas = document.getElementById("canvas");
 
 //-- Definir el tamaño del canvas
 
-canvas.height = 900;
-canvas.width = 600;
+canvas.height = 650;
+canvas.width = 900;
 
 //-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
@@ -13,49 +13,96 @@ const ctx = canvas.getContext("2d");
 let x = (canvas.width - 60) / 2; // Centrado horizontalmente
 let y = canvas.height - 35; // Abajo en el canvas
 
+let em1x = 130;
+let em1y = 200;
+
 //-- Velocidades del objeto
-let velx = 3;
-let vely = 1;
+let velocidad_movimiento = 4;
+let velocidad_disparo = 10;
+let velocidad_enemigos = 5;
+requestAnimationFrame(enemigos);
 
 dibujarP(x, y, 60, 30, "blue"); 
 //-- Función principal de animación
-function update() 
-{
-  console.log("test");
-  //-- Algoritmo de animación:
-  //-- 1) Actualizar posición del  elemento
-  //-- (física del movimiento rectilíneo uniforme)
 
-  //-- Condición de rebote en extremos verticales del canvas
-  if (x < 0 || x >= (canvas.width - 20)) {
-    rebote_sound.currentTime = 0;
-    rebote_sound.play();
-    velx = -velx;
+function enemigos() {
+  dibujarP(em1x, em1y, 60, 30, "red");
+  dibujarP(em1x + 70, em1y, 60, 30, "red"); 
+  dibujarP(em1x + 140, em1y, 60, 30, "red"); 
+  dibujarP(em1x + 210, em1y, 60, 30, "red"); 
+  dibujarP(em1x + 280, em1y, 60, 30, "red");
+  dibujarP(em1x + 350, em1y, 60, 30, "red");
+  dibujarP(em1x + 420, em1y, 60, 30, "red");
+  dibujarP(em1x + 490, em1y, 60, 30, "red");
+  dibujarP(em1x + 560, em1y, 60, 30, "red");
+  dibujarP(em1x, em1y - 40, 60, 30, "red");
+  dibujarP(em1x + 70, em1y - 40, 60, 30, "red");
+  dibujarP(em1x + 140, em1y - 40, 60, 30, "red");
+  dibujarP(em1x + 210, em1y - 40, 60, 30, "red");
+  dibujarP(em1x + 280, em1y - 40, 60, 30, "red");
+  dibujarP(em1x + 350, em1y - 40, 60, 30, "red");
+  dibujarP(em1x + 420, em1y - 40, 60, 30, "red");
+  dibujarP(em1x + 490, em1y - 40, 60, 30, "red");
+  dibujarP(em1x + 560, em1y - 40, 60, 30, "red");
+  dibujarP(em1x, em1y - 80, 60, 30, "red");
+  dibujarP(em1x + 70, em1y - 80, 60, 30, "red");
+  dibujarP(em1x + 140, em1y - 80, 60, 30, "red");
+  dibujarP(em1x + 210, em1y - 80, 60, 30, "red");
+  dibujarP(em1x + 280, em1y - 80, 60, 30, "red");
+  dibujarP(em1x + 350, em1y - 80, 60, 30, "red");
+  dibujarP(em1x + 420, em1y - 80, 60, 30, "red");
+  dibujarP(em1x + 490, em1y - 80, 60, 30, "red");
+  dibujarP(em1x + 560, em1y - 80, 60, 30, "red");
 
-    // Evitar que traspase las paredes laterales
-    x = Math.max(0, Math.min(x, canvas.width - 20));
-  }
+  requestAnimationFrame(moverse_enemigos);
 
-  //-- Condición de rebote en extremos horizontales del canvas
-  if (y <= 0 || y > 80) {
-    rebote_sound.currentTime = 0;
-    rebote_sound.play();
-    vely = -vely;
-
-    // Evitar que traspase las paredes horizontales
-    y = Math.max(0, Math.min(y, 80));
-  }
-
-  //-- Actualizar la posición
-  x = x + velx;
-  y = y + vely;
-
-  //-- 2) Borrar el canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
-  //-- 4) Volver a ejecutar update cuando toque
-  requestAnimationFrame(update);
 }
+
+function moverse_enemigos() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpiar todo el canvas
+  em1x += velocidad_enemigos; // Mover a la derecha
+  if (em1x + 630 >= canvas.width) {
+    em1x = 0; // Reiniciar la posición del enemigo al lado izquierdo
+    if (em1y >= canvas.height) {
+      velocidad_enemigos = 0; // Detener el movimiento del enemigo
+    } else {
+      em1y += 40; // Cambiar la posición vertical del enemigo
+    }
+  }
+
+  enemigos(); // Redibujar enemigos
+}
+
+function iniciar() {
+  dibujarP(x, y, 60, 30, "blue"); // Pintar el proyectil
+  moverse(); // Llamar a la función de actualización
+  //console.log("test");
+}
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "ArrowRight") {
+
+    if (x >= (canvas.width - 64)) {
+      x = (canvas.width - 64);
+    }
+    else {
+      x += velocidad_movimiento; // Mover a la derecha
+    }
+
+  }
+  else if (event.key === "ArrowLeft") {
+    if (x <= 4) {
+      x = 4;
+    }
+    else {
+      x -= velocidad_movimiento; // Mover a la izquierda
+    }
+  }
+  ctx.clearRect(0, y, canvas.width, 30);
+
+});
+
+
 
 function dibujarP(x,y,lx,ly,color) {
 
@@ -75,6 +122,8 @@ function dibujarP(x,y,lx,ly,color) {
   ctx.stroke();
 
   ctx.closePath();
+  requestAnimationFrame(iniciar);
+
 }
 
 
